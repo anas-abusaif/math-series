78352f30b2380915d2a43d5553f08b876f88dea5
anas-abusaif <anasabusief@gmail.com>
2021-10-11 17:08:23 +0300

    test1

---
+++ math_series/series.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+   return 0
+
+
+
+def lucas(n):
+  return 2
+
+
+def sum_series(n,p1,p2):
+  if n==0:
+    return 0
+  elif n==2:
+    return 2
---
+++ tests/test_math_series.py
@@ -0,0 +1,23 @@
+from math_series import __version__
+from math_series.series import fibonacci,lucas,sum_series
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_fibonacci_0():
+      actual = fibonacci(0)
+      expected = 0
+      assert actual == expected
+
+
+def test_licas_0():
+      actual = lucas(0)
+      expected = 2
+      assert actual == expected
+
+def test_sum_series_0():
+      actual = lucas(2)
+      expected = 2
+      assert actual == expected
+
