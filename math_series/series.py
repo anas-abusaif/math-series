98f5a0a34a1e4930a7ebd4d86cc231cade14bc6c
anas-abusaif <anasabusief@gmail.com>
2021-10-11 18:15:53 +0300

    test3

--- math_series/series.py
+++ math_series/series.py
@@ -1,14 +1,25 @@
 def fibonacci(n):
-   return 0
-
+   if n==0:
+      return 0
+   elif n==1:
+        return 1
+   else:
+        return fibonacci(n-1)+fibonacci(n-2)
 
 
 def lucas(n):
-  return 2
+    if n==0:
+        return 2
+    elif n==1:
+        return 1
+    else:
+        return lucas(n-1)+lucas(n-2)
 
 
-def sum_series(n,p1,p2):
-  if n==0:
-    return 0
-  elif n==2:
-    return 2
+def sum_series(n,p1=0,p2=1):
+    if n==p1:
+        return p1
+    elif n==p2:
+        return p2
+    else:
+        return sum_series(n-1)+sum_series(n-2)
